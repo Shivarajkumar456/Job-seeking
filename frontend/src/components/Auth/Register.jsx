5d66ff0e829f605b1f0042430a9e72bb5c8ecdb0
shivarajkumar456 <shivaraj1654@gmail.com>
2024-07-23 23:51:15 +0530

    backend setup

--- frontend/src/components/Auth/Register.jsx
+++ frontend/src/components/Auth/Register.jsx
@@ -73,7 +73,7 @@ const Register = () => {
               <div>
                 <input
                   type="text"
-                  placeholder="Zeeshan"
+                  placeholder="Shivaraj"
                   value={name}
                   onChange={(e) => setName(e.target.value)}
                 />
@@ -85,7 +85,7 @@ const Register = () => {
               <div>
                 <input
                   type="email"
-                  placeholder="zk@gmail.com"
+                  placeholder="srk@gmail.com"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                 />
